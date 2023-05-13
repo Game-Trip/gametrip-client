239c96806c469ccc6d6a2bf13442237166deffe5
Dercraker <antoine.capitain@gmail.com>
2023-05-13 19:27:01 +0200

    Get Translation

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -9,9 +9,32 @@ import { IconButton, InputAdornment } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import React from "react";
 import { Link } from "react-router-dom";
+import {useEffect, useState} from 'react'
 
 const HomePage: React.FC = () => {
   const isPresent = useIsPresent();
+
+  const [searchText, setSearchText] = useState("");
+  let language: string = "fr_FR";
+
+  const fetchText = async () => {
+    const response = await fetch(
+      `https://cdn.simplelocalize.io/9543c8c2e15a41bfb022955f49c0aab8/_latest/${language}`
+    );
+    const translations = await response.json();
+    setSearchText(translations["Home.Search"]);
+  };
+
+  useEffect(() => {
+        fetchText();
+  }, []);
+
+
+
+  const handleChange = (e: any) => {
+    language = e.target.value;
+    fetchText();
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -33,11 +56,18 @@ const HomePage: React.FC = () => {
             <span>Login</span>
           </Link>
         </div>
+        <div>
+      <select onChange={handleChange}>
+        <option value="en_US">English</option>
+        <option value="fr_FR" selected>Français</option>
+      </select>
+    </div>
         <img className={styles.image} src={logo} />
 
         <div className={styles.footer}>
           <div className={styles.searchSection}>
             <div className={styles.basicText}>Recherchez un jeu</div>
+            <div className={styles.basicText}>{searchText}</div>
             <div
               className={css`
                 display: flex;
