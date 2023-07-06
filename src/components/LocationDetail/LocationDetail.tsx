db8c4c152bbaefa2232ace6b9894c8cc595dea39
Seddik Mehdi <mhdi.seddik@gmail.com>
2023-07-06 14:29:49 +0200

    add apk build pipeline

--- src/components/LocationDetail/LocationDetail.tsx
+++ src/components/LocationDetail/LocationDetail.tsx
@@ -8,16 +8,18 @@ type Props = {
 export default function LocationDetail({ locationDto }: Props) {
     return (
         <>
-            <div>{locationDto?.name}</div>
             <div className={styles.flex}>
                 <img className={styles.image} src="https://i.ytimg.com/vi/xeHkYv04zbQ/maxresdefault.jpg" />
                 <img className={styles.image} src="https://www.1jour1actu.com/wp-content/uploads/2021/10/VIDEO_histoire_tour_Eiffel.jpeg" />
             </div>
-            <div>{locationDto?.games?.map((game) => {
-                return (<div>
-                    <div>{game.name}</div>
-                </div>)
-            })}</div>
+            Related games
+            <div className={styles.tagList}>
+                {locationDto?.games?.map((game) => {
+                    return (
+                        <div className={styles.tag}>{game.name}</div>
+                    )
+                })}
+            </div>
         </>
     )
 }
@@ -26,8 +28,29 @@ const styles = {
     flex: css`
         display: flex;
         flex-direction: row;
+        /* center */
+        justify-content: center;
+        align-items: center;
+        gap: 50px;
+        margin-top: 50px;
     `,
     image: css`
         width: 40%;
+        height: 400px;
+    `,
+    tagList: css`
+    margin-top: 10px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    margin-bottom:10px;
+  `,
+    tag: css`
+    background: #85d8ac;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px 5px;
+    border-radius: 8px;
     `,
 }
